8df4c92322c1a39b77af502e2e58baa21451600e
damian mazzini <damian.mazzini@sondeos.com.ar>
2020-07-23 23:42:02 -0300

    tercerpaso

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ new Vue({
             this.enJuego = true
             this.jugador = 100
             this.monstruo = 100 
+        },
+        atacar: function() {
+            let danio = 7
+
+            this.jugador -= danio
+            this.monstruo -= danio 
+
+            if(this.monstruo <= 0) {
+                alert('Ganaste!')
+                this.enJuego = false
+            }
+        },
+        ataqueEspecial: function() {
+
+        },
+        curar: function() {
+
+        },
+        rendirse: function() {
+
         }
     }
 }
