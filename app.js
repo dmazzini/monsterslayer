72037a5d447dec28cecb3b538a6e803139dcefeb
damian mazzini <damian.mazzini@sondeos.com.ar>
2020-07-25 20:27:19 -0300

    se agrega style y boton de atacar

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:'#app',
+    data: {
+        jugador: 100,
+        monstruo: 100
+    },
+    methods: {
+        atacar: function() {
+            this.jugador = this.jugador -10
+            this.monstruo = this.monstruo -10
+        }
+    }
+}
+)
